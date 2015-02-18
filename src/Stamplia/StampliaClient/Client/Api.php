b6ad323c4c37018d9c91320059e339478f1cb564
jfoucher <jfoucher@6px.eu>
2015-02-18 12:20:12 +0100

    add upload method to upload zip file

--- src/Stamplia/StampliaClient/Client/Api.php
+++ src/Stamplia/StampliaClient/Client/Api.php
@@ -9,6 +9,7 @@
 
 namespace Stamplia\StampliaClient\Client;
 
+use Guzzle\Http\EntityBody;
 use Stamplia\StampliaClient\Exception\StampliaApiException;
 use Stamplia\StampliaClient\Provider\Stamplia;
 use Guzzle\Http\StaticClient as GuzzleClient;
@@ -201,7 +202,7 @@ class Api {
                 'namespace' => 'litmustests',
             ),
             'postZip' => array(
-                'method' => 'post',
+                'method' => 'upload',
                 'url' => '/users/{userId}/zips',
                 'parameters' => array('userId', 'file'),
             ),
@@ -442,6 +443,18 @@ class Api {
                     }
                     
                     return $response->getBody();
+
+                case 'upload':
+                    $request = $client->post(
+                        $url,
+                        array(
+                            'Accept' => 'application/json',
+                            'Authorization' => 'Bearer '.$this->accessToken,
+                        ),
+                        array('file'=>'@'.$data['file'])
+                    );
+                    $response = $request->send();
+                    break;
                     
             }
 
